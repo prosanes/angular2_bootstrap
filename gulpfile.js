cca9f2d7f8713cd88ca0582a964191f498af847b
Pedro Rosanes <prosanes@gmail.com>
2016-07-03 14:25:27 -0300

    Fix dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,10 @@ var gulp = require('gulp'),
     gulp_jspm = require('gulp-jspm');
 
 gulp.task('default', function() {
-  return gulp.src('lib/main.js')
-    .pipe(gulp_jspm({
-        selfExecutingBundle: true
-      }))
-    .pipe(gulp.dest('build/'));
-});
-
-gulp.task('angular', function() {
   return gulp.src('app/bootstrap.ts')
     .pipe(gulp_jspm({
-        selfExecutingBundle: true
+        selfExecutingBundle: true,
+        minify: true
       }))
     .pipe(gulp.dest('build/'));
 });
