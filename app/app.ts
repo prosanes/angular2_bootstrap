a6cbd33059e959ddf5132b4930b4a86144daab88
Pedro Rosanes <prosanes@gmail.com>
2016-07-05 00:23:15 -0300

    Using JSPM 0.17@beta

---
+++ app/app.ts
@@ -0,0 +1,9 @@
+import 'systemjs-hot-reloader/default-listener.js';
+
+import {Component} from "angular2/core"
+
+@Component({
+  selector: "my-app",
+  template: `test5`
+})
+export class App { }
---
+++ app/main.ts
@@ -0,0 +1,9 @@
+import "zone.js";
+import "reflect-metadata";
+import 'systemjs-hot-reloader/default-listener.js';
+
+import {bootstrap} from "angular2/platform/browser";
+import {App} from "./app";
+
+bootstrap(App);
+
